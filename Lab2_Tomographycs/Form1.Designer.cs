18d3ee15aeebcf27f0902536609d7e771ed48e47
SandalovKY <sandaloff.constantin@yandex.ru>
2020-04-20 16:28:52 +0400

    second commit

---
+++ Lab2_Tomographycs/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Lab2_Tomographycs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Enabled = false;
+            this.glControl1.Location = new System.Drawing.Point(13, 13);
+            this.glControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(923, 569);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = true;
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(255, 605);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(681, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 605);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "LOAD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(943, 43);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(307, 56);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(943, 122);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(307, 56);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(952, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Transfer Function Minimum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(952, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Transfer Function Width";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Texture",
+            "Quads",
+            "Quad Strip"});
+            this.comboBox1.Location = new System.Drawing.Point(955, 202);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Lab2_Tomographycs/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_Tomographycs
+{
+    public partial class Form1 : Form
+    {
+        Bin bin;
+        View view;
+        bool loaded;
+        int currentLayer;
+        int frameCount;
+        DateTime NextFPSUpdate;
+        bool needReload;
+        protected void displayFPS()
+        {
+            if (DateTime.Now >= NextFPSUpdate)
+            {
+                this.Text = String.Format("CT Vizualizer (fps = {0})", frameCount);
+                NextFPSUpdate = DateTime.Now.AddSeconds(1);
+                frameCount = 0;
+                trackBar2.Maximum = 2000;
+                trackBar3.Maximum = 4000;
+                trackBar3.Minimum = 1;
+                trackBar2.Value = view.min;
+                trackBar3.Value = view.width;
+            }
+            frameCount++;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            bin = new Bin();
+            view = new View();
+            loaded = false;
+            currentLayer = 0;
+            frameCount = 0;
+            NextFPSUpdate = DateTime.Now.AddSeconds(1);
+            needReload = true;
+            comboBox1.SelectedIndex = 0;
+            MessageBox.Show(comboBox1.SelectedIndex.ToString() + '\n' + comboBox1.SelectedItem);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Idle += Application_Idle;
+        }
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            draw();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            currentLayer = trackBar1.Value;
+            needReload = true;
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            while (glControl1.IsIdle)
+            {
+                displayFPS();
+                glControl1.Invalidate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string name = dialog.FileName;
+                Bin.readBIN(name);
+                view.SetupView(glControl1.Width, glControl1.Height);
+                loaded = true;
+                glControl1.Invalidate();
+                trackBar1.Maximum = Bin.Z - 1;
+            }
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            needReload = true;
+            view.width = trackBar3.Value;
+            draw();
+        }
+        void draw()
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        if (loaded)
+                        {
+                            if (needReload)
+                            {
+                                view.generateTextureImage(currentLayer);
+                                view.Load2DTexture();
+                                needReload = false;
+                            }
+                            view.DrawTexture();
+                            glControl1.SwapBuffers();
+                        }
+                        break;
+                    }
+                case 1:
+                    {
+                        if (loaded)
+                        {
+                            view.DrawQuads(currentLayer);
+                            glControl1.SwapBuffers();
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        if (loaded)
+                        {
+                            view.DrawQuadStrip(currentLayer);
+                            glControl1.SwapBuffers();
+                        }
+                        break;
+                    }
+            }
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            needReload = true;
+            view.min += trackBar2.Value - view.min;
+            draw();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
